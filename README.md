b2df63c1a118b0877eddaed411dc33b6592f574a
Ben Coveney <bencoveney@gmail.com>
2024-01-21 15:33:49 +0000

    Sample SVG generation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rudiments
+
+http://lilypond.org/doc/v2.25/Documentation/usage/basic-command_002dline-options-for-lilypond
+
+```bash
+lilypond --svg -dcrop=#f --output="./assets/" ./rudiments/sample.ly
+```
---
+++ src/rudiments.ts
@@ -0,0 +1,19 @@
+export type Rudiment = {
+  name: string;
+  pattern: string;
+};
+
+export const rudiments: Rudiment[] = [
+  {
+    name: "Single Stroke Roll",
+    pattern: "R16-L16-R16-L16-R16-L16-R16-L16-R16-L16-R16-L16-R16-L16-R16-L16",
+  },
+  {
+    name: "Single Stroke Four",
+    pattern: "RT8-LT8-RT8-L4-RT8-LT8-RT8-L4-R8",
+  },
+  {
+    name: "Single Stroke Seven",
+    pattern: "RT8-LT8-RT8-L4-RT8-LT8-RT8-L4-R8",
+  },
+];
