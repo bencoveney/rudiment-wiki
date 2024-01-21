3bef9acaf10225cf6c4f0108a5b57ea00085bf8e
Ben Coveney <bencoveney@gmail.com>
2024-01-21 18:03:57 +0000

    Add some other rudiments

---
+++ rudiments/double-stroke-roll.ly
@@ -0,0 +1,18 @@
+\version "2.22.1"
+
+rudiment = \drummode {
+  \repeat unfold 4 {
+    sn16^"R" 16^"R" sn16^"L" 16^"L"
+  }
+}
+
+\score {
+  <<
+    \new DrumStaff <<
+      \override Score.SystemStartBar.collapse-height = #-inf.0
+      \omit Staff.TimeSignature
+      \omit Staff.Clef
+      \new DrumVoice { \stemUp \rudiment }
+    >>
+  >>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,33 @@
-import { writeFile } from "fs/promises";
+import { writeFile, readdir, mkdir } from "fs/promises";
+import { resolve, relative } from "path";
+import { exec } from "child_process";
+import { promisify } from "util";
 import React from "react";
 import { renderToStaticMarkup } from "react-dom/server";
+
+const RUDIMENTS_DIR = "rudiments/";
+const BUILD_DIR = "build";
+const SVG_DIR = "assets/";
+
+const execPromise = promisify(exec);
+async function buildRudiment(rudimentFile: string, outputDir: string) {
+  const command = `
+    lilypond --svg -dcrop=#t -dpoint-and-click=#f --output="${outputDir}" ${rudimentFile};
+  `.trim();
+  console.log(command);
+  const { stderr } = await execPromise(command);
+  console.log(stderr);
+}
+
+async function buildRudiments(outputDir: string) {
+  const base = resolve(process.cwd(), RUDIMENTS_DIR);
+  const files = await readdir(base);
+  await Promise.all(
+    files.map((file) =>
+      buildRudiment(relative(process.cwd(), resolve(base, file)), outputDir)
+    )
+  );
+}
 
 function buildHtml() {
   const content = renderToStaticMarkup(
@@ -13,6 +40,15 @@ function buildHtml() {
 
       <body>
         <h1>Rudiments</h1>
+        <img
+          src="./assets/single-stroke-roll.cropped.svg"
+          alt="Single Stroke Roll"
+        />
+        <img
+          src="./assets/double-stroke-roll.cropped.svg"
+          alt="Double Stroke Roll"
+        />
+        <img src="./assets/paradiddle.cropped.svg" alt="Paradiddle" />
       </body>
     </html>
   );
@@ -20,6 +56,9 @@ function buildHtml() {
 }
 
 async function writePage(content: string) {
+  const svgDir = resolve(BUILD_DIR, SVG_DIR);
+  await mkdir(svgDir, { recursive: true });
+  await buildRudiments(svgDir);
   await writeFile("./build/index.html", content, "utf-8");
 }
 
